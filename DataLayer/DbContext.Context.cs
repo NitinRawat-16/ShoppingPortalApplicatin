1f5fbbed9da836ca3fd2dbeccdf7acf3ddb46aa4
Nitin Rawat <nitinr426@gmail.com>
2022-04-06 10:49:01 +0530

    changing namespace of tt file in DataLayer

--- DataLayer/DbContext.Context.cs
+++ DataLayer/DbContext.Context.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DataLayer
+namespace Model
 {
     using System;
     using System.Data.Entity;
